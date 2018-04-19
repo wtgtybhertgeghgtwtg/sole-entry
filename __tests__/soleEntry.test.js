9ce167012d2eaf4707de981f3a321c80867014cb
wtgtybhertgeghgtwtg <wtgtybhertgeghgtwtg@gmail.com>
2018-04-18 20:13:59 -0700

    feat: allow custom error message

--- __tests__/soleEntry.test.js
+++ __tests__/soleEntry.test.js
@@ -2,21 +2,30 @@
 import soleEntry from '../src';
 
 describe('soleEntry', () => {
-  it('throws if "obj" is undefined.', () => {
-    // $FlowFixMe
-    expect(() => soleEntry()).toThrow('"obj" must be an object.');
-  });
+  describe('invariants', () => {
+    it('throws if "obj" is undefined.', () => {
+      // $FlowFixMe
+      expect(() => soleEntry()).toThrow('"obj" must be an object.');
+    });
 
-  it('throws if "obj" is null.', () => {
-    // $FlowFixMe
-    expect(() => soleEntry(null)).toThrow('"obj" must be an object.');
-  });
+    it('throws if "obj" is not an object.', () => {
+      // $FlowFixMe
+      expect(() => soleEntry('Not an object')).toThrow(
+        '"obj" must be an object.',
+      );
+    });
 
-  it('throws if "obj" is not an object.', () => {
-    // $FlowFixMe
-    expect(() => soleEntry('Not an object')).toThrow(
-      '"obj" must be an object.',
-    );
+    it('throws if "obj" is null.', () => {
+      // $FlowFixMe
+      expect(() => soleEntry(null)).toThrow('"obj" must be an object.');
+    });
+
+    it('throws if "message" is not a string or undefined.', () => {
+      // $FlowFixMe
+      expect(() => soleEntry({foo: 1}, 1)).toThrow(
+        '"message" must be a string or undefined.',
+      );
+    });
   });
 
   it('throws if "obj" has no properties.', () => {
@@ -29,6 +38,11 @@ describe('soleEntry', () => {
     expect(() => soleEntry({foo: 1, bar: 'two'})).toThrow(
       '"obj" must have one and only one property.',
     );
+  });
+
+  it('throws with "message" if provided.', () => {
+    const message = 'Some custom message.';
+    expect(() => soleEntry({}, message)).toThrow(message);
   });
 
   it('returns the sole entry of the input.', () => {
